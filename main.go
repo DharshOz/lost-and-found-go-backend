9defb1a898bbada89efbecf5f503a9ced431e85c
DharshOz <jayanthirajendran111@gmail.com>
2025-04-29 18:58:23 +0530

    changed *

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 
 	// CORS configuration
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
